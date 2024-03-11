ea9dc186550a069c93c25c0977941683b8a9ccee
Mhirii <ahmedmhiri218@gmail.com>
2024-03-11 03:57:54 +0100

    launch projects

---
+++ project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+verbose=1
+rofi=0
+if [ "$1" == "rofi" ]; then
+	rofi=1
+fi
+
+cd "$HOME/projects/launch/" || exit
+project_launchers=$HOME/projects/launch
+
+list=$(ls | grep -v launch.sh)
+if [ $verbose -eq 1 ]; then
+	echo projects list:
+	for i in $list; do
+		echo -e "\033[0;32m$i\033[0m"
+	done
+fi
+
+if [ $rofi -eq 1 ]; then
+	selected_project=$(echo $list | tr " " "\n" | rofi -dmenu -p "Select project: ")
+else
+	selected_project=($(echo $list | tr " " "\n" | fzf --preview "cat {}" --preview-window=right:60%:wrap --height=40% --border --prompt="Select project: "))
+fi
+
+# run the selected script
+if [ -n "$selected_project" ]; then
+	echo -e "\033[0;32mRunning $selected_project\033[0m"
+	"$project_launchers/$selected_project"
+fi
